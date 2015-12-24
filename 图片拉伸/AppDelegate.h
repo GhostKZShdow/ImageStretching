fdb7fbfd8535d23c671ab7e15ac0da394536fb5f
我 <KZ@zhangkundemacbook-pro.local>
2015-12-24 12:07:48 +0800

    Initial Commit

---
+++ 图片拉伸/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片拉伸
+//
+//  Created by 张坤 on 15/12/24.
+//  Copyright © 2015年 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片拉伸/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片拉伸
+//
+//  Created by 张坤 on 15/12/24.
+//  Copyright © 2015年 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
